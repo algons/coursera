14823623992c0a6b9c09b4a6547d5bef973807bd
agondiken <alper.gondiken@visualdna.com>
2014-07-27 00:46:39 +0100

    updated shiny app

---
+++ Developing Data Products/execute.R
@@ -0,0 +1,7 @@
+library("shiny")
+runApp("~/Documents/study/Developing Data Products/shiny_project")
+
+
+library("shinyapps")
+deployApp("~/Documents/study/Developing Data Products/shiny_project/")
+Y
--- Developing Data Products/server.R
+++ Developing Data Products/server.R
@@ -1,18 +1,23 @@
 library("shiny")
 
-tipModel <- function(bill,feedback){
-  #Essentially every positive feedback counts for 5% tip
-  positive.feedbacks <- length(feedback)
-  tip <- bill*positive.feedbacks*0.05
+tipModel <- function(bill,feedback,size){
+  #Essentially every positive feedback counts for 5% tip on top of 5% minimum tip (social norm)
+  feedback.reward <- length(feedback)*0.05 + 0.05
+  #Every incremental person in the group reduces the bill by 1%
+  size.penalty <- (1 - size*0.01)
+  #People tend to pay percentage-wise less tips when the bill is higher
+  bill.penalty <- (1 - bill*0.001)
+  tip <- round(bill*feedback.reward*size.penalty*bill.penalty,1)
   tip
 }
 
 shinyServer(
   function(input,output){
     output$o.bill <- renderPrint({input$bill})
+    output$o.size <- renderPrint({input$size})
     output$o.feedback <- renderPrint({input$feedback})
-    output$tip <- renderPrint({tipModel(input$bill,input$feedback)})
-    output$total <- renderPrint({input$bill + tipModel(input$bill,input$feedback)})
+    output$tip <- renderPrint({tipModel(input$bill,input$feedback,input$size)})
+    output$total <- renderPrint({input$bill + tipModel(input$bill,input$feedback,input$size)})
   }
 )
 
